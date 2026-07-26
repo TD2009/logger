f73fe85f0146ad5ceb183bc592d9b00047d3f141
Tanish Dasari <110691492+TD2009@users.noreply.github.com>
2026-07-26 18:07:53 -0500

    Refactor esp_profiler.h by cleaning up code
    Removed unused includes and synchronization telemetry structure.

--- main/esp_profiler.h
+++ main/esp_profiler.h
@@ -1,35 +1,16 @@
 #ifndef ESP_PROFILER_H
 #define ESP_PROFILER_H
 
-#include <stdio.h>
 #include <stdint.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/semphr.h"
-#include "esp_timer.h"
-#include "esp_system.h"
-#include "esp_heap_caps.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+/**
+ * @brief Get current hardware timestamp in microseconds.
+ */
+uint64_t esp_profiler_get_time_us(void);
 
-// --- Synchronization Telemetry Structure ---
-typedef struct {
-    volatile uint64_t total_wait_time_us;
-    volatile uint64_t max_wait_time_us;
-    volatile uint32_t acquisitions;
-} sem_profiler_stats_t;
-
-extern sem_profiler_stats_t g_sem_stats;
-
-// Core Profiler Functions
-void esp_profiler_init(void);
-void esp_profiler_run_window(uint32_t window_ms);
-BaseType_t xSemaphoreTakeProfiled(SemaphoreHandle_t xSemaphore, TickType_t xTicksToWait);
-
-#ifdef __cplusplus
-}
-#endif
+/**
+ * @brief Perform active spin-wait loop simulating task execution.
+ */
+void esp_profiler_busy_wait_us(uint32_t us);
 
 #endif // ESP_PROFILER_H
